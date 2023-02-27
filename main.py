bcdb3a2545577438023413c299cdf355b9a82de6
Dylan Schultz <hello@schultzie.dev>
2023-02-26 19:59:32 -0800

    formatting
    Signed-off-by: Dylan Schultz <hello@schultzie.dev>

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ RPC: str
 
 logger = logging.getLogger(__name__)
 stream_handler = logging.StreamHandler()
-formatter = logging.Formatter("%(asctime)s | %(levelname)s | %(message)s")
+formatter = logging.Formatter("%(levelname)s | %(asctime)s | %(message)s")
 stream_handler.setLevel(logging.INFO)
 stream_handler.setFormatter(formatter)
 logger.addHandler(stream_handler)
