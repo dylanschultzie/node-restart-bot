cd467f988c10983d2d9a11e651ae37293a974668
Dylan Schultz <hello@schultzie.dev>
2023-02-27 07:07:30 -0800

    Fix datetime format
    Signed-off-by: Dylan Schultz <hello@schultzie.dev>

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ def handle_lost_peers() -> tuple:
 def handle_stalled(catching_up: bool, latest_block_time: str) -> tuple:
     message: str = ""
     status: str = ""
-    block_time = datetime.strptime(latest_block_time, "%Y-%m-%dT%I:%M:%S")
+    block_time = datetime.strptime(latest_block_time, "%Y-%m-%dT%H:%M:%S")
     if not catching_up and node_stalled(block_time):
         status = subprocess.run(f"systemctl restart { DAEMON }", shell=True)
         message = f"❌ | node: { RPC } | stalled, node restarted"
