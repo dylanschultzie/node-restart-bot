7cf5178c5ebe02109ba1e073e32471b6c5bc68a2
Dylan Schultz <hello@schultzie.dev>
2023-02-26 19:26:47 -0800

    Fix logging
    Signed-off-by: Dylan Schultz <hello@schultzie.dev>

--- main.py
+++ main.py
@@ -11,6 +11,12 @@ STALL_MINUTES: int
 DISCORD_WEBHOOK: SyncWebhook
 RPC: str
 
+logger = logging.getLogger(__name__)
+stream_handler = logging.StreamHandler()
+stream_handler.setLevel(logging.DEBUG)
+logger.addHandler(stream_handler)
+logger.setLevel(logging.INFO)
+
 def get_status_info(sync_info: Dict) -> tuple:
     catching_up = sync_info["catching_up"]
     latest_block_time = sync_info["latest_block_time"]
